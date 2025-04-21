25ddbc4c66046e31ae43b9bb3adb07d1e2755f40
LANDNh <landon.hurst@outlook.com>
2025-04-21 07:50:11 -0400

    error clearing

--- backend/web/index.php
+++ backend/web/index.php
@@ -12,3 +12,6 @@ require dirname(__DIR__) . '/bootstrap.php';
 /** @var craft\web\Application $app */
 $app = require CRAFT_VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
 $app->run();
+
+phpinfo();
+die();
