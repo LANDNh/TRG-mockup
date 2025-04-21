d49f749eed1821b1184574337e4d647b11bb286d
LANDNh <landon.hurst@outlook.com>
2025-04-21 06:44:29 -0400

    more env variables for live and in general.php

--- backend/web/index.php
+++ backend/web/index.php
@@ -2,6 +2,9 @@
 /**
  * Craft web bootstrap file
  */
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_WARNING);
 
 // Load shared bootstrap
 require dirname(__DIR__) . '/bootstrap.php';
